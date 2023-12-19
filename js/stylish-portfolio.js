aad5e16b41e6e35f6ba9e901844257cfa6f67997
Elijah Romashchenko <elijahro93@gmail.com>
2023-12-19 14:42:29 -0400

    change php file + HTML froms to execute mail.php

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,37 +1,65 @@
-(function($) {
-  "use strict"; // Start of use strict
+(function ($) {
+  'use strict'; // Start of use strict
+
+  function showMessage(message, isError) {
+    var popupClass = isError ? 'error' : 'success';
+    var $popup = $(
+      '<div class="popup ' + popupClass + '">' + message + '</div>'
+    );
+
+    $('body').append($popup);
+
+    // Remove the popup after a few seconds
+    setTimeout(function () {
+      $popup.animate({ opacity: 0 }, 500, function () {
+        $popup.remove();
+      });
+    }, 3000);
+  }
 
   // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
+  $('.menu-toggle').click(function (e) {
     e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-    $(this).toggleClass("active");
+    $('#sidebar-wrapper').toggleClass('active');
+    $('.menu-toggle > .fa-bars, .menu-toggle > .fa-times').toggleClass(
+      'fa-bars fa-times'
+    );
+    $(this).toggleClass('active');
   });
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, "easeInOutExpo");
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          'easeInOutExpo'
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
-    $("#sidebar-wrapper").removeClass("active");
-    $(".menu-toggle").removeClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  $('#sidebar-wrapper .js-scroll-trigger').click(function () {
+    $('#sidebar-wrapper').removeClass('active');
+    $('.menu-toggle').removeClass('active');
+    $('.menu-toggle > .fa-bars, .menu-toggle > .fa-times').toggleClass(
+      'fa-bars fa-times'
+    );
   });
 
   // Scroll to top button appear
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
@@ -39,5 +67,4 @@
       $('.scroll-to-top').fadeOut();
     }
   });
-
 })(jQuery); // End of use strict
--- mail.php
+++ mail.php
@@ -1,16 +1,31 @@
-<?php
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
-$email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
+<?
+echo "connected!"
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) $name = htmlspecialchars($_POST['name']);
+    if (isset($_POST['email'])) $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    if (isset($_POST['message'])) $message = htmlspecialchars($_POST['message']);
+    if (isset($_POST['subject'])) $subject = htmlspecialchars($_POST['subject']);
 
-$content="From: $name \n Email: $email \n Message: $message";
-$recipient = "luda.bogoslov.1968@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
-?>
+    // Basic input validation
+    if (empty($name) || empty($email) || empty($message) || empty($subject)) {
+        die("Error: All fields are required.");
+    }
+
+    $content = "From: $name \n Email: $email \n Message: $message";
+    // $recipient = "luda.bogoslov.1968@gmail.com";
+
+    $recipient = "romashbis@gmail.com"; // for testing purpose
+
+    $mailheader = "From: $email \r\n";
+
+    // Try sending the email and handle errors
+    if (mail($recipient, $subject, $content, $mailheader)) {
+        echo "Email sent!";
+        // Example success message
+        echo '<script>showMessage("Email sent!", false);</script>';
+    } else {
+        echo "Error: Unable to send email.";
+        echo '<script>showMessage("Error: Unable to send email.", true);</script>';
+
+    }
+}?>
